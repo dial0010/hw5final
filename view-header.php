bab45502cf6160c163a6542270ae3a5861e00232
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-12-08 23:43:39 -0600

    Create view-header.php

---
+++ view-header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drivers and Violations</title>
+
+    <!-- Include Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Include custom CSS -->
+    <link href="css/style.css" rel="stylesheet" />
+
+    <!-- Include jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <!-- Include Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <!-- Include custom JS -->
+    <script src="js/script.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Drivers & Violations</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="view-drivers.php">Drivers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="view-violations.php">Violations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="view-drivers-with-violation.php">Drivers with Violations</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
